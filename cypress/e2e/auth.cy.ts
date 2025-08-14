b2e496a39de03b8de254ca8213b87306f73e7850
RaduStefan30 <radustefan3010@gmail.com>
2025-08-14 12:38:24 +0300

    Move strings from specs

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,20 +1,21 @@
-import users from '../fixtures/users.json'
-import { LoginPage } from '../pages/LoginPage'
-import { ProductsPage } from '../pages/ProductsPage'
+import users from "../fixtures/users.json";
+import constants from "../fixtures/constants.json";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductsPage } from "../pages/ProductsPage";
 
-const login = new LoginPage()
-const products = new ProductsPage()
+const login = new LoginPage();
+const products = new ProductsPage();
 
-describe('Authentication', () => {
-    it('Test case 1: shows expected error for a non-valid user (locked account)', () => {
-        login.visit()
-        login.loginAs(users.locked.username, users.locked.password)
-        login.expectErrorContains('locked out')
-    })
+describe("Authentication", () => {
+    it("Test case 1: shows expected error for a non-valid user (locked account)", () => {
+        login.visit();
+        login.loginAs(users.locked.username, users.locked.password);
+        login.expectErrorContains(constants.messages.lockedOut);
+    });
 
-    it('Test case 2: logs in successfully with a valid user', () => {
-        login.visit()
-        login.loginAs(users.valid.username, users.valid.password)
-        products.expectOnPage()
-    })
-})
+    it("Test case 2: logs in successfully with a valid user", () => {
+        login.visit();
+        login.loginAs(users.valid.username, users.valid.password);
+        products.expectOnPage();
+    });
+});
--- cypress/e2e/checkout.cy.ts
+++ cypress/e2e/checkout.cy.ts
@@ -1,39 +1,41 @@
-import users from '../fixtures/users.json'
-import { LoginPage } from '../pages/LoginPage'
-import { ProductsPage } from '../pages/ProductsPage'
-import { Header } from '../pages/Header'
-import { CartPage } from '../pages/CartPage'
-import { CheckoutPage } from '../pages/CheckoutPage'
+import users from "../fixtures/users.json";
+import constants from "../fixtures/constants.json";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductsPage } from "../pages/ProductsPage";
+import { Header } from "../pages/Header";
+import { CartPage } from "../pages/CartPage";
+import { CheckoutPage } from "../pages/CheckoutPage";
 
-const login = new LoginPage()
-const products = new ProductsPage()
-const header = new Header()
-const cart = new CartPage()
-const checkout = new CheckoutPage()
+const login = new LoginPage();
+const products = new ProductsPage();
+const header = new Header();
+const cart = new CartPage();
+const checkout = new CheckoutPage();
 
-describe('Checkout flow', () => {
-    it('Test case 4: buys an item and completes checkout', () => {
-        const itemName = 'Sauce Labs Backpack'
+describe("Checkout flow", () => {
+    it("Test case 4: buys an item and completes checkout", () => {
+        const itemName = "Sauce Labs Backpack";
+        const { firstName, lastName, postalCode } = constants.user;
 
-        login.visit()
-        login.loginAs(users.valid.username, users.valid.password)
-        products.expectOnPage()
+        login.visit();
+        login.loginAs(users.valid.username, users.valid.password);
+        products.expectOnPage();
 
-        products.addToCartByName(itemName)
-        header.expectCartBadge(1)
-        header.openCart()
+        products.addToCartByName(itemName);
+        header.expectCartBadge(1);
+        header.openCart();
 
-        cart.expectOnPage()
-        cart.expectItemPresent(itemName)
-        cart.checkout()
+        cart.expectOnPage();
+        cart.expectItemPresent(itemName);
+        cart.checkout();
 
-        checkout.fill('Radu', 'QA', '400000')
-        checkout.continue()
+        checkout.fill(firstName, lastName, postalCode);
+        checkout.continue();
 
-        checkout.expectOnPage()
-        checkout.expectItemPresent(itemName)
-        checkout.finish()
+        checkout.expectOnPage();
+        checkout.expectItemPresent(itemName);
+        checkout.finish();
 
-        checkout.expectOrderComplete()
-    })
-})
+        checkout.expectOrderComplete();
+    });
+});
